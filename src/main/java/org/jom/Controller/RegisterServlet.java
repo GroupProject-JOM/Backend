a8e8c90de750f2fab512d97d20a0ed0b57cb9b61
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-09-27 01:01:54 +0530

    bad request method added

--- src/main/java/org/jom/Controller/RegisterServlet.java
+++ src/main/java/org/jom/Controller/RegisterServlet.java
@@ -1,10 +1,8 @@
 package org.jom.Controller;
 
-import com.google.gson.Gson;
 import org.jom.Model.UserModel;
+import com.google.gson.Gson;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +14,10 @@ import java.util.regex.Pattern;
 @WebServlet("/signup")
 public class RegisterServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("Inside");
-        response.setContentType("application/json");
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-
         try {
             Gson gson = new Gson();
-
             // json data to user object
             BufferedReader bufferedReader = request.getReader();
             UserModel user = gson.fromJson(bufferedReader, UserModel.class);
@@ -38,69 +33,68 @@ public class RegisterServlet extends HttpServlet {
 
 //        UserModel user = new UserModel(first_name,last_name,email,password,phone,address1,address2,address3);
 
-            System.out.println(user.getFirst_name());
-            System.out.println(user.getLast_name());
-            System.out.println(user.getEmail());
-            System.out.println(user.getPassword());
-            System.out.println(user.getPhone());
-            System.out.println(user.getAdd_line_1());
-            System.out.println(user.getAdd_line_2());
-            System.out.println(user.getAdd_line_3());
+//            System.out.println(user.getFirst_name());
+//            System.out.println(user.getLast_name());
+//            System.out.println(user.getEmail());
+//            System.out.println(user.getPassword());
+//            System.out.println(user.getPhone());
+//            System.out.println(user.getAdd_line_1());
+//            System.out.println(user.getAdd_line_2());
+//            System.out.println(user.getAdd_line_3());
 
             // Check input field is empty
             if(user.getFirst_name().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("First name cannot be empty!");
-                return;
+                return ;
             }if(user.getLast_name().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Last name cannot be empty!");
                 return;
             }
             if(user.getEmail().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Email cannot be empty!");
                 return;
             }
             if(user.getPassword().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Password cannot be empty!");
                 return;
             }
             if(user.getPhone().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Contact number cannot be empty!");
                 return;
             }
             if(user.getAdd_line_1().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Address line 1 cannot be empty!");
                 return;
             }
             if(user.getAdd_line_2().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Address line 2 cannot be empty!");
                 return;
             }
             if(user.getAdd_line_3().isEmpty()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Address line 3 cannot be empty!");
                 return;
             }
 
             // Email validation
-            String regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
+            String regex = "[a-z0-9]+@[a-z]+\\.[a-z]{2,3}";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(user.getEmail());
-
             if(!matcher.matches()){
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 out.write("Enter a valid email!");
+                System.out.println("Invalid email");
                 return;
             }
 
-
-
+            // All validations are passed then register
             if(user.Register()){
                 response.setStatus(HttpServletResponse.SC_OK);
                 out.write("Registration successfully");
