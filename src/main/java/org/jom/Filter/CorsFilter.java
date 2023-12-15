e709df3d7819574d18223febd628314c7de5645b
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-12-15 11:50:52 +0530

    Allow two origins

--- src/main/java/org/jom/Filter/CorsFilter.java
+++ src/main/java/org/jom/Filter/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         // Add CORS headers here
-        httpResponse.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); // Replace with your frontend origin
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5501,http://127.0.0.1:5502"); // Replace with your frontend origin
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
