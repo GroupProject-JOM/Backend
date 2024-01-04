e9c25fd6c22e58706bccc9254ad84ef01f2ba78c
Buddhika Nipun <bnsbuddhika@gmail.com>
2024-01-04 14:33:21 +0530

    View distributor
    Get single distributor personal data and remaining products amount , their dao , queries and constructors

--- src/main/java/org/jom/Controller/SalesManager/DistributorsServlet.java
+++ src/main/java/org/jom/Controller/SalesManager/DistributorsServlet.java
@@ -2,9 +2,11 @@ package org.jom.Controller.SalesManager;
 
 import com.google.gson.Gson;
 import org.jom.Auth.JwtUtils;
+import org.jom.Dao.DistributionDAO;
 import org.jom.Dao.Supplier.Collection.SupplyDAO;
 import org.jom.Dao.UserDAO;
 import org.jom.Model.Collection.SupplyModel;
+import org.jom.Model.DistributionModel;
 import org.jom.Model.UserModel;
 import org.json.JSONObject;
 
@@ -13,6 +15,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -45,7 +48,8 @@ public class DistributorsServlet extends HttpServlet {
                     break;  // No need to continue checking if "jwt" cookie is found
                 }
             }
-        } else {response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             out.write("{\"message\": \"UnAuthorized\"}");
             System.out.println("No cookies found in the request.");
             return;
@@ -99,5 +103,102 @@ public class DistributorsServlet extends HttpServlet {
         } finally {
             out.close();
         }
+    }
+
+    //Get single Distributor's products
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        // Get all cookies from the request
+        Cookie[] cookies = request.getCookies();
+        JSONObject jsonObject = new JSONObject();
+        int user_id = 0;
+        boolean jwtCookieFound = false;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwt".equals(cookie.getName())) {
+                    JwtUtils jwtUtils = new JwtUtils(cookie.getValue());
+                    if (!jwtUtils.verifyJwtAuthentication()) {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        out.write("{\"message\": \"UnAuthorized\"}");
+                        System.out.println("UnAuthorized1");
+                        return;
+                    }
+                    jsonObject = jwtUtils.getAuthPayload();
+                    jwtCookieFound = true;
+                    break;  // No need to continue checking if "jwt" cookie is found
+                }
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            out.write("{\"message\": \"UnAuthorized\"}");
+            System.out.println("No cookies found in the request.");
+            return;
+        }
+
+        // If "jwt" cookie is not found, respond with unauthorized status
+        if (!jwtCookieFound) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            out.write("{\"message\": \"UnAuthorized - JWT cookie not found\"}");
+            System.out.println("UnAuthorized - JWT cookie not found");
+            return;
+        }
+
+        user_id = (int) jsonObject.get("user");
+
+        try {
+            StringBuilder requestBody = new StringBuilder();
+
+            try (BufferedReader reader = request.getReader()) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    requestBody.append(line);
+                }
+            }
+
+            JSONObject jsonData = new JSONObject(requestBody.toString());
+            int distributor_id = jsonData.getInt("distributor");
+
+            UserDAO userDAO = new UserDAO();
+            UserModel user = userDAO.getUserById(user_id);
+
+            if (user.getId() != 0) {
+                if (user.getRole().equals("sales-manager")) {
+
+                    DistributionDAO distributionDAO = new DistributionDAO();
+                    List<DistributionModel> allocated = distributionDAO.DistributorsRemaining(distributor_id);
+
+                    Gson gson = new Gson();
+                    String objectArray = gson.toJson(allocated); // Object array to json
+
+                    if (allocated.size() != 0) {
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        out.write("{\"allocated\": " + objectArray + "}");
+                        System.out.println("Send allocated");
+                    } else {
+                        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+                        out.write("{\"allocated\": \"0\"}");
+                        System.out.println("No allocated");
+                    }
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    out.write("{\"message\": \"Invalid User\"}");
+                    System.out.println("Invalid User");
+                }
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                out.write("{\"message\": \"Invalid User\"}");
+                System.out.println("Invalid User");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            out.close();
+        }
+
     }
 }
--- src/main/java/org/jom/Model/DistributionModel.java
+++ src/main/java/org/jom/Model/DistributionModel.java
@@ -5,11 +5,22 @@ public class DistributionModel {
     private String first_name;
     private String last_name;
     private int remaining;
+    private String type;
+    private String category;
+    private int product;
 
     public DistributionModel(int id, String first_name, String last_name, int remaining) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
         this.remaining = remaining;
+    }
+
+    public DistributionModel(int id, int remaining, String type, String category, int product) {
+        this.id = id;
+        this.remaining = remaining;
+        this.type = type;
+        this.category = category;
+        this.product = product;
     }
 }
