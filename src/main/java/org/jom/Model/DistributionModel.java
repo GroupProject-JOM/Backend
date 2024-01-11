9e2dd36cc56c8c3177d5487f41e4149633bc7d69
Buddhika Nipun <bnsbuddhika@gmail.com>
2024-01-11 18:34:38 +0530

    Distribute to outlets processes
    Distributions table insert queries and dao functions , distributor products decrement on products distributions table and distribution servlet post function

--- src/main/java/org/jom/Model/DistributionModel.java
+++ src/main/java/org/jom/Model/DistributionModel.java
@@ -9,12 +9,22 @@ public class DistributionModel {
     private String category;
     private int product;
     private String price;
+    private int outlet;
+    private int distributor;
 
     public DistributionModel(int id, String first_name, String last_name, int remaining) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
         this.remaining = remaining;
+    }
+
+    public DistributionModel(int remaining, int product, String price, int outlet, int distributor) {
+        this.remaining = remaining;
+        this.product = product;
+        this.price = price;
+        this.outlet = outlet;
+        this.distributor = distributor;
     }
 
     public DistributionModel(int id, int remaining, String type, String category, int product) {
@@ -31,5 +41,85 @@ public class DistributionModel {
         this.category = category;
         this.product = product;
         this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(int remaining) {
+        this.remaining = remaining;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getProduct() {
+        return product;
+    }
+
+    public void setProduct(int product) {
+        this.product = product;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getOutlet() {
+        return outlet;
+    }
+
+    public void setOutlet(int outlet) {
+        this.outlet = outlet;
+    }
+
+    public int getDistributor() {
+        return distributor;
+    }
+
+    public void setDistributor(int distributor) {
+        this.distributor = distributor;
     }
 }
