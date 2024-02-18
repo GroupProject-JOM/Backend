4b5c7449c173657efc71434f81919a3ccb9b62e5
Buddhika Nipun <bnsbuddhika@gmail.com>
2024-02-19 01:54:48 +0530

    Get all upcoming collections and get all past collections on relevant collector functions and related model constructors

--- src/main/java/org/jom/Controller/Collector/CollectorServlet.java
+++ src/main/java/org/jom/Controller/Collector/CollectorServlet.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 @WebServlet("/collector")
 public class CollectorServlet extends HttpServlet {
+    //Get collector dashboard data
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
@@ -116,6 +117,172 @@ public class CollectorServlet extends HttpServlet {
                     } else {
                         response.setStatus(HttpServletResponse.SC_ACCEPTED);
                         out.write("{\"size\": " + today_collections.size() + ",\"today\":" + today_collec + ",\"count\":" + today_count + ",\"rate\": " + object + "}");
+                        System.out.println("No upcoming collections");
+                    }
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    out.write("{\"message\": \"Invalid User\"}");
+                    System.out.println("Invalid User");
+                }
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                out.write("{\"message\": \"Invalid User\"}");
+                System.out.println("Invalid User");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            out.close();
+        }
+    }
+
+    // get all past collections
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        // Get all cookies from the request
+        Cookie[] cookies = request.getCookies();
+        JSONObject jsonObject = new JSONObject();
+        int employee_id = 0;
+        boolean jwtCookieFound = false;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwt".equals(cookie.getName())) {
+                    JwtUtils jwtUtils = new JwtUtils(cookie.getValue());
+                    if (!jwtUtils.verifyJwtAuthentication()) {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        out.write("{\"message\": \"UnAuthorized\"}");
+                        System.out.println("UnAuthorized1");
+                        return;
+                    }
+                    jsonObject = jwtUtils.getAuthPayload();
+                    jwtCookieFound = true;
+                    break;  // No need to continue checking if "jwt" cookie is found
+                }
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            out.write("{\"message\": \"UnAuthorized\"}");
+            System.out.println("No cookies found in the request.");
+            return;
+        }
+
+        // If "jwt" cookie is not found, respond with unauthorized status
+        if (!jwtCookieFound) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            out.write("{\"message\": \"UnAuthorized - JWT cookie not found\"}");
+            System.out.println("UnAuthorized - JWT cookie not found");
+            return;
+        }
+
+        employee_id = (int) jsonObject.get("sId");
+
+        try {
+            EmployeeDAO employeeDAO = new EmployeeDAO();
+            EmployeeModel collector = employeeDAO.getEmployee(employee_id);
+
+            if (collector.geteId() != 0) {
+                if (collector.getRole().equals("collector")) {
+                    SupplyDAO supplyDAO = new SupplyDAO();
+
+                    List<SupplyModel> past_collections = supplyDAO.getAllPastCollections(employee_id);
+
+                    Gson gson = new Gson();
+                    String past = gson.toJson(past_collections); // Object array to json
+
+                    if (past_collections.size() != 0) {
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        out.write("{\"list\": " + past + "}");
+                        System.out.println("Send all past collections");
+                    } else {
+                        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+                        out.write("{\"message\": \"No past collections\"}");
+                        System.out.println("No past collections");
+                    }
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    out.write("{\"message\": \"Invalid User\"}");
+                    System.out.println("Invalid User");
+                }
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                out.write("{\"message\": \"Invalid User\"}");
+                System.out.println("Invalid User");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            out.close();
+        }
+    }
+
+    // get all upcoming collections
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        // Get all cookies from the request
+        Cookie[] cookies = request.getCookies();
+        JSONObject jsonObject = new JSONObject();
+        int employee_id = 0;
+        boolean jwtCookieFound = false;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwt".equals(cookie.getName())) {
+                    JwtUtils jwtUtils = new JwtUtils(cookie.getValue());
+                    if (!jwtUtils.verifyJwtAuthentication()) {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        out.write("{\"message\": \"UnAuthorized\"}");
+                        System.out.println("UnAuthorized1");
+                        return;
+                    }
+                    jsonObject = jwtUtils.getAuthPayload();
+                    jwtCookieFound = true;
+                    break;  // No need to continue checking if "jwt" cookie is found
+                }
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            out.write("{\"message\": \"UnAuthorized\"}");
+            System.out.println("No cookies found in the request.");
+            return;
+        }
+
+        // If "jwt" cookie is not found, respond with unauthorized status
+        if (!jwtCookieFound) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            out.write("{\"message\": \"UnAuthorized - JWT cookie not found\"}");
+            System.out.println("UnAuthorized - JWT cookie not found");
+            return;
+        }
+
+        employee_id = (int) jsonObject.get("sId");
+
+        try {
+            EmployeeDAO employeeDAO = new EmployeeDAO();
+            EmployeeModel collector = employeeDAO.getEmployee(employee_id);
+
+            if (collector.geteId() != 0) {
+                if (collector.getRole().equals("collector")) {
+                    SupplyDAO supplyDAO = new SupplyDAO();
+
+                    List<SupplyModel> upcoming_collections = supplyDAO.getAllUpcomingCollections(employee_id);
+
+                    Gson gson = new Gson();
+                    String upcoming = gson.toJson(upcoming_collections); // Object array to json
+
+                    if (upcoming_collections.size() != 0) {
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        out.write("{\"list\": " + upcoming + "}");
+                        System.out.println("Send all upcoming collections");
+                    } else {
+                        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+                        out.write("{\"message\": \"No past collections\"}");
                         System.out.println("No upcoming collections");
                     }
                 } else {
